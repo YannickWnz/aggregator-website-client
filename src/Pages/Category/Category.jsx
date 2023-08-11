8ad36ca5b7ca16433cf9bbfd97eb5cd88e13418f
yannickwnz <yannickwnz@gmail.com>
2023-08-11 08:44:11 +0000

    local news fetched

--- src/Pages/Category/Category.jsx
+++ src/Pages/Category/Category.jsx
@@ -7,49 +7,44 @@ import './Category.scss'
 
 export const Category = () => {
 
+    // Extract the 'category' parameter from the URL
     const {category} = useParams() 
 
-    console.log(category)
-
+    // State variables
     const [results, setResults] = useState([])
+    const [localNewsResults, setLocalNewsResults] = useState([])
     const [articlesToShow, setArticlesToShow] = useState(5);
     const articlesIncrement = 5;
 
-
+    // Function to fetch search results from the NewsAPI
     const searchApiCall = async () => {
-
         try {
-
-            if(category === 'ghana') return
-            
-            const response = await axios.get(`https://newsapi.org/v2/everything?q=${category.toLocaleLowerCase()}&apiKey=91c897b5e5534d609204e6fd90fd0b25`)
-
-            setResults(response.data.articles)
-            
-        } catch (error) {
-            console.log(error)
-        }
-
-    }
-
-    const getLocalNews = async () => {
-        try {
-
-            if (!category === 'ghana') {
-                return
-            } else {
-                const response = await axios.get('https://content.guardianapis.com/search?q=ghana&api-key=2257749e-0fbd-42dc-8063-f500316ffa36')
+            if(category !== 'ghana') {
+                // const response = await axios.get(`https://newsapi.org/v2/everything?q=${category.toLocaleLowerCase()}&apiKey=91c897b5e5534d609204e6fd90fd0b25`)
+                const response = await axios.get(`https://newsapi.org/v2/everything?q=${category.toLocaleLowerCase()}&apiKey=9060ec5bd2414ffa81465607bc541985`)
                 setResults(response.data.articles)
             }
-
         } catch (error) {
             console.log(error)
         }
     }
 
+    // Function to fetch local news using The Guardian API
+    const getLocalNews = async () => {
+        try {
+            if(category === 'ghana') {
+                const response = await axios.get('https://content.guardianapis.com/search?q=ghana&api-key=2257749e-0fbd-42dc-8063-f500316ffa36')
+                setLocalNewsResults(response.data.response.results)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    // Effect to run the API calls based on the 'category' parameter
     useEffect(() => {
-        searchApiCall()
-        getLocalNews()
+        // searchApiCall()
+        // getLocalNews()
     }, [category])
 
     // handle load more function
@@ -57,6 +52,7 @@ export const Category = () => {
         setArticlesToShow(prevArticlesToShow => prevArticlesToShow + articlesIncrement)
     }
 
+    // Function to generate the category name with proper capitalization
     function setCategoryName(category) {
         if(category === 'general') {
             return 'World'
@@ -69,9 +65,27 @@ export const Category = () => {
     return (
         <div className="search">
             <div className="search-container">
+                {/* Display category name if there are search results */}
                 {results.length > 0 && <h1>{setCategoryName(category)} News</h1>}
 
-                { results.length > 0 ? results.slice(0, articlesToShow).map((result, index) => {
+                {localNewsResults.length > 0 && localNewsResults.map((result, index) => {
+                    return (
+                        <div className="news" key={index}>
+                            <div className="news-description">
+                                <p className="news-source">{result.pillarName}</p>
+                                <a href={result.webUrl} target='_blank' className="news-headline">{result.webTitle}</a>
+                                <p className="news-post-time">{articlePostTime(result.webPublicationDate)}</p>
+                            </div>
+                            {/* <div 
+                                className="news-image"
+                                style={ results.length > 0 ? { backgroundImage:  `url(${result.urlToImage})` } : ''}
+                            ></div> */}
+                        </div>
+                    )
+                }) }
+
+                {/* Display search results */}
+                {results.length > 0 && results.slice(0, articlesToShow).map((result, index) => {
                     return (
                         <div className="news" key={index}>
                             <div className="news-description">
@@ -85,7 +99,10 @@ export const Category = () => {
                             ></div>
                         </div>
                     )
-                } ) : <div className="no-results"><h1>No results found</h1></div> }
+                } )}
+
+                {/* show no results message if no data was returned from api  */}
+                {localNewsResults.length === 0 && results.length === 0 && <div className="no-results"><h1>No results found</h1></div>}
 
                 {results.length > 0 && <div className="load-more-btn">
                     <button onClick={handleLoadMore}
